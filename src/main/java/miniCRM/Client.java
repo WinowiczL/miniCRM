174f4a22bb85d0b906ab2431f47fbe614f6e020d
Lukasz Winowicz <winowiczlukasz@gmail.com>
2018-03-31 14:14:06 +0200

    src added

---
+++ src/main/java/miniCRM/Client.java
@@ -0,0 +1,13 @@
+package miniCRM;
+
+public class Client {
+
+	private String firstName;
+	private String secondName;
+	private String description;
+	private String dateOfSession;
+	private String typeOfSession;
+	private int priceOfSession;
+	private int valueOfClient;
+	
+}
